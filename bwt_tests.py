f94c299b580221be8c0da8a35e798bfe8b3c7a99
Rowen Felt <rfelt@middlebury.edu>
2019-05-10 12:13:26 -0400

    fucked beyond belief

--- bwt_tests.py
+++ bwt_tests.py
@@ -6,6 +6,7 @@ Adapted from bwt_query_exercise_tests.py by Michael Linderman
 
 import unittest
 import bwt_query
+import sparse_bwt_query
 
 class TestBWTFMIndex(unittest.TestCase):
     def setUp(self):
@@ -74,6 +75,112 @@ class TestBWTQuery(unittest.TestCase):
         matches = self.query_mismatch("CAG", 2)
         self.assertEqual(sorted(matches), [0, 2, 3, 4, 9, 10, 12, 13, 14, 15, 19])
 
+    def test_mismatches3(self):
+        matches = self.query_mismatch("GGGG", 1)
+        self.assertEqual(sorted(matches), [3, 4, 13, 14, 15]) 
+
+    def test_mismatches4(self):
+        matches = self.query_mismatch("GGGG", 2)
+        self.assertEqual(sorted(matches), [2, 3, 4, 5, 12, 13, 14, 15, 16])
+
+#class TestBWTQuery(unittest.TestCase):
+#    def setUp(self):
+#        # Before each test construct FM index data structures for this genome
+#        self.text = "panamabananas"
+#        self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix = bwt_query.build_indices(self.text)
+#    
+#    def query(self, pattern):
+#        """ Query pre-indexed genome for pattern, returning list of matching start indices """
+#        return bwt_query.query(self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix, pattern)
+#
+#    def query_mismatch(self, pattern, mismatches):
+#        """Query with mismatches"""
+#        return bwt_query.query_mismatch(self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix, pattern, mismatches)
+#        
+#    def test_mismatches1(self):
+#        matches = self.query_mismatch("nan", 1)
+#        self.assertEqual(sorted(matches), [0, 2, 6, 8, 10])
+#
+#class TestBWTSparsity1(unittest.TestCase):
+#    def setUp(self):
+#        # Before each test construct FM index data structures for this genome
+#        self.text = "panamabananas"
+#        self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix = sparse_bwt_query.build_sparse_indices(self.text, 5)
+#        
+#    def query_mismatch(self, pattern, mismatches, sparsity):
+#        """Query with mismatches"""
+#        return sparse_bwt_query.query_mismatch(self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix, pattern, mismatches, sparsity)
+#
+#    def test_sparse_count_matrix(self):
+#        columns = []
+#        test = [["$", [0,0,0]], ["a", [0,0,3]], ["b", [0,1,1]], ["m", [0,1,1]], ["n", [0,1,3]], ["p", [0,1,1]], ["s", [0,1,1]]]
+#        for key in self.count_matrix.keys():
+#            columns.append([key, self.count_matrix[key]])
+#        self.assertEqual(sorted(columns, key=lambda x: x[0]), test)
+#    
+#    def test_sparse_matches1(self):
+#        matches = self.query_mismatch("nan", 0, 5)
+#        self.assertEqual(sorted(matches), [8])
+#
+#    def test_sparse_matches2(self):
+#        matches = self.query_mismatch("an", 0, 5)
+#        self.assertEqual(sorted(matches), [1, 7, 9])
+#
+#    def test_sparse_matches3(self):
+#        matches = self.query_mismatch("pan", 0, 5)
+#        self.assertEqual(sorted(matches), [0])
+#
+#    def test_sparse_matches3(self):
+#        matches = self.query_mismatch("nanas", 0, 5)
+#        self.assertEqual(sorted(matches), [8])
+#
+#    ### matches appears to be working
+#        
+#    def test_sparse_mismatches2(self):
+#        matches = self.query_mismatch("nan", 1, 5)
+#        self.assertEqual(sorted(matches), [0, 2, 6, 8, 10])
+    
+#class TestBWTQuery(unittest.TestCase):
+#    def setUp(self):
+#        # Before each test construct FM index data structures for this genome
+#        self.text = "AATCGGGTTCAATCGGGGTCAG"
+#        self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix = sparse_bwt_query.build_sparse_indices(self.text, 5)
+#    
+#    def query(self, pattern, sparsity):
+#        """ Query pre-indexed genome for pattern, returning list of matching start indices """
+#        return sparse_bwt_query.query(self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix, pattern, sparsity)
+#
+#    def query_mismatch(self, pattern, mismatches, sparsity):
+#        """Query with mismatches"""
+#        return sparse_bwt_query.query_mismatch(self.letters, self.suffix_array, self.first_occur, self.last_col, self.count_matrix, pattern, mismatches, sparsity)
+#    
+#    def test_sparse_query(self):
+#        """ Query with multiple matches """
+#        # Query is a method of this class, and so is invoked via self
+#        matches = self.query("ATCG", 5)
+#        # Check equality of actual matches to expected matches (sorting to avoid ambiguity)
+#        self.assertEqual(sorted(matches), [1, 11])
+#
+#    def test_sparse_query2(self):
+#        """ Query end match """
+#        matches = self.query("GGGT", 5)
+#        self.assertEqual(sorted(matches), [4, 15])
+#
+#    def test_sparse_mismatches1(self):
+#        matches = self.query_mismatch("TCC", 1, 5)
+#        self.assertEqual(sorted(matches), [2, 7, 8, 12, 18])
+#
+#    def test_sparse_mismatches2(self):
+#        matches = self.query_mismatch("CAG", 2, 5)
+#        self.assertEqual(sorted(matches), [0, 2, 3, 4, 9, 10, 12, 13, 14, 15, 19])
+#
+#    def test_sparse_mismatches3(self):
+#        matches = self.query_mismatch("GGGG", 1, 5)
+#        self.assertEqual(sorted(matches), [3, 4, 13, 14, 15]) 
+#
+#    def test_sparse_mismatches4(self):
+#        matches = self.query_mismatch("GGGG", 2, 5)
+#        self.assertEqual(sorted(matches), [2, 3, 4, 5, 12, 13, 14, 15, 16])
 
 if __name__ == '__main__':
     unittest.main()
--- sparse_bwt_query.py
+++ sparse_bwt_query.py
@@ -55,8 +55,32 @@ def build_count_matrix(letters, last_column):
                 letter_list.append(letter_list[-1])    
     return count_matrix
 
+def build_sparse_count_matrix(letters, last_column, sparsity):
+    """ Build count matrix from last_column
     
-def build_indices(text):
+    Args:
+        letters: String of letters in index
+        last_column: Last column as string
+        sparsity: factor by which to reduce count_matrix
+        
+    Return:
+        Dictionary of count array indexed by symbol with sparsity
+    """ 
+    count_matrix = {}
+    count_letters = {}
+    for letter in letters:
+        count_matrix[letter] = []
+        count_letters[letter] = 0
+    i = 0
+    for current_letter in last_column:
+        if i % sparsity == 0:
+            for letter in letters:
+                count_matrix[letter].append(count_letters[letter])
+        i+=1
+        count_letters[current_letter] = count_letters[current_letter] + 1 #sum of letters
+    return count_matrix
+
+def build_sparse_indices(text, sparsity):
     """Build BWT FM-index data structures
     
     Args:
@@ -85,28 +109,31 @@ def build_indices(text):
     first_col = "".join([text[cycle[0]] for cycle in cycles])
     letters, first_occur = build_first_occur(first_col)
 
-    count_matrix = build_count_matrix(letters, last_col)
+    count_matrix = build_sparse_count_matrix(letters, last_col, sparsity)
         
     return (letters, suffix_array, first_occur, last_col, count_matrix)
 
-def query(letters, suffix_array, first_occur, last_col, count_matrix, pattern):
-    """ A wrapper for querying without mismatches """
-    return query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, 0, None, None)
 
-def query_mismatch(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches):
+def query(letters, suffix_array, first_occur, last_col, count_matrix, pattern, sparsity):
+    """ A wrapper for querying without mismatches """
+    return query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, 0, None, None, sparsity)
+
+def query_mismatch(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches, sparsity):
     """ A wrapper for querying with mismatches """
+    if mismatches == 0:
+        return query(letters, suffix_array, first_occur, last_col, count_matrix, pattern, sparsity)
     matches = set()
-    base = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches, None, None) 
+    base = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches, None, None, sparsity) 
     for match in base:
         matches.add(match)
     new_pattern = pattern[:-1]
-    temp = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, new_pattern, mismatches-1, None, None)
+    temp = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, new_pattern, mismatches-1, None, None, sparsity)
     for match in temp:
         matches.add(match)
     return matches
     
 
-def query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches, top, bot):   
+def query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, pattern, mismatches, top, bot, sparsity):   
     """ Query BWT FM-index
     
     Args:
@@ -119,6 +146,7 @@ def query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, patter
         mismatches: number of allowed mismatches
         top: top of current view of BWT in recursive call, None if first call
         bot: bottom of current view of BWT in recursive call, None if first call
+        sparsity: sparsity factor of checkpoint matrix
     
     Returns:
         List of match positions given a number of mismatches, last letter cannot be a mismatch
@@ -137,28 +165,56 @@ def query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, patter
             for letter in letters:
                 if letter == "$": #if it's the $ skip it
                     continue
-                count_array = count_matrix[letter]
-                if count_array[bot] - count_array[top] != 0 and top != 0 and count_array[top-1] != count_array[top]:    #if the letter appears in this range
+                new_top, new_bot = calculate_sparse_count(letters, letter, i, first_occur, count_matrix, top, bot, last_col, sparsity)
+                if new_top != None and new_bot != None:
                     new_pattern = pattern[:i] + letter          #create new pattern with letter 
-                    first = first_occur[letters.find(letter)]   
-                    new_top = first + count_array[top]
-                    new_bot = first + count_array[bot]
-                    new_matches = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, new_pattern, mismatches-1, new_top, new_bot)
+                    new_matches = query_bwt(letters, suffix_array, first_occur, last_col, count_matrix, new_pattern, mismatches-1, new_top, new_bot, sparsity)
                     for match in new_matches:
                         matches.add(match)
             return matches
         else: #no more mismatches
-            first = first_occur[letters.find(pattern[i])] # index of first occurrence in first_col
-            # Generate "next" top and bottom pointers using count array
-            count_array = count_matrix[pattern[i]] #count array for a given symbol
-            top = first + count_array[top] 
-            bot = first + count_array[bot]
+            top, bot = calculate_sparse_count(letters, pattern[i], i, first_occur, count_matrix, top, bot, last_col, sparsity)
             i -= 1
 
     for suffix in suffix_array[top:bot]:        #add direct matches
         matches.add(suffix)
-
     return list(matches)
+
+def calculate_sparse_count(letters, letter, index, first_occur, count_matrix, top, bot, last_col, sparsity):
+    """ Calculates the new top and bottom pointers using the sparse count matrix 
+    
+    Args: 
+        letters: String of letters in index
+        suffix_array: SuffixArray as list
+        first_occur: List of first occurrences
+        last_col: Last column of BW matrix as string
+        count_matrix: Counts as a dictionary
+        top: top of current view of BWT in recursive call, None if first call
+        bot: bottom of current view of BWT in recursive call, None if first call
+        sparsity: sparsity factor of checkpoint matrix
+    
+    Returns:
+        new top and bottom pointers
+        returns None, None if the given letter does not appear in the range
+    """
+    first = first_occur[letters.find(letter)] # index of first occurrence in first_col
+    # Generate "next" top and bottom pointers using count array
+    count_array = count_matrix[letter] #count array for a given symbol
+    top_const = count_array[top//sparsity] #find most recently populated row
+                                        #analagous to count_array[bot] in non-sparse count matrix
+    for i in range(1 + (top//sparsity) * sparsity, top+1):
+        if last_col[i-1] == letter:
+            top_const += 1   
+    bot_const = count_array[bot//sparsity] #analagous to count_array[bot] in non-sparse count matrix
+    for i in range(1 + (bot//sparsity) * sparsity, bot+1):
+        if last_col[i-1] == letter:
+            bot_const += 1   
+    if bot_const - top_const == 0 and top != 0 and last_col[top-1] != letter: #if the letter does not appear in this range argv
+        # might also need something like x and 
+        return None, None
+    top = first + top_const
+    bot = first + bot_const
+    return top, bot
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
